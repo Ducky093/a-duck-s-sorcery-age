1e0fea0108bc1088d985456b14e49fae9d3a7d47
BRUHSIF38 <bruhsif38@gmail.com>
2025-09-20 14:47:04 -0400

    it was 100 per tick.

--- src/main/java/radon/jujutsu_kaisen/ability/ratio/Collapse.java
+++ src/main/java/radon/jujutsu_kaisen/ability/ratio/Collapse.java
@@ -192,7 +192,7 @@ public class Collapse extends Ability implements Ability.IChannelened, Ability.I
 
     @Override
     public float getCost(LivingEntity owner) {
-        return 100.0F;
+        return 20.0F;
     }
 
     @Override
