52312ca2cfd09cbaa8509c6cbbb3fd632865894d
wood-m-corp <148374240+wood-m-corp@users.noreply.github.com>
2025-09-23 04:18:29 -0400

    Overlay fixes for CS and BT
    as stated

--- src/main/java/radon/jujutsu_kaisen/client/visual/overlay/BrainTransplantOverlay.java
+++ src/main/java/radon/jujutsu_kaisen/client/visual/overlay/BrainTransplantOverlay.java
@@ -3,6 +3,7 @@ package radon.jujutsu_kaisen.client.visual.overlay;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.OverlayTexture;
@@ -10,11 +11,12 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import radon.jujutsu_kaisen.JujutsuKaisen;
 import radon.jujutsu_kaisen.capability.data.sorcerer.CursedTechnique;
+import radon.jujutsu_kaisen.client.JJKRenderTypes;
 import radon.jujutsu_kaisen.client.visual.ClientVisualHandler;
 import radon.jujutsu_kaisen.client.visual.base.IOverlay;
 
 public class BrainTransplantOverlay implements IOverlay {
-    private static final RenderType BRAIN_TRANSPLANT = RenderType.entityCutoutNoCull(new ResourceLocation(JujutsuKaisen.MOD_ID, "textures/overlay/brain_transplant.png"));
+    private static final RenderType BRAIN_TRANSPLANT = JJKRenderTypes.eyes(new ResourceLocation(JujutsuKaisen.MOD_ID, "textures/overlay/brain_transplant.png"));
 
     @Override
     public boolean isValid(LivingEntity entity, ClientVisualHandler.ClientData data) {
@@ -24,7 +26,11 @@ public class BrainTransplantOverlay implements IOverlay {
     @Override
     public <T extends LivingEntity> void render(T entity, ClientVisualHandler.ClientData data, ResourceLocation texture, EntityModel<T> model, PoseStack poseStack, MultiBufferSource buffer, float partialTicks, int packedLight) {
         VertexConsumer consumer = buffer.getBuffer(BRAIN_TRANSPLANT);
-        model.renderToBuffer(poseStack, consumer, packedLight, OverlayTexture.NO_OVERLAY,
+        model.renderToBuffer(poseStack, consumer, LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY,
                 1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
+
+
+
+
