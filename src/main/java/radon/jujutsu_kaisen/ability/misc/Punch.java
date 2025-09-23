4a244dc14bed80aeede0d318f1668ca8e8729bcf
BRUHSIF38 <bruhsif38@gmail.com>
2025-09-22 21:01:13 -0400

    whatever i hope this fixes flight

--- src/main/java/radon/jujutsu_kaisen/ability/misc/Punch.java
+++ src/main/java/radon/jujutsu_kaisen/ability/misc/Punch.java
@@ -112,10 +112,10 @@ public class Punch extends Ability implements Ability.ICharged{
             if (mod != 1) {
                 mod = (float) owner.getDeltaMovement().length();
                 Vec3 look2 = look.normalize().scale(power*mod+1.5);
-                if (look2.y < 0) {
+                if (look2.y > 0) {
                     look2.multiply(0.8,0.4,0.8);
                 } else {
-                    look2.multiply(1.2,1.2,1.2);
+                    look2.multiply(1.2,0.4,1.2);
                 }
                 owner.setDeltaMovement(look2.x,look2.y,look2.z);
             }
