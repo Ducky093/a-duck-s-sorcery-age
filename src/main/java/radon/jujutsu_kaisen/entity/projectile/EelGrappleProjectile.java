df174bc18d0337e29d18d889cc56aea9d9f226fe
BRUHSIF38 <bruhsif38@gmail.com>
2025-10-04 20:56:43 -0400

    Update EelGrappleProjectile.java

--- src/main/java/radon/jujutsu_kaisen/entity/projectile/EelGrappleProjectile.java
+++ src/main/java/radon/jujutsu_kaisen/entity/projectile/EelGrappleProjectile.java
@@ -4,6 +4,8 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.effect.MobEffectInstance;
+import radon.jujutsu_kaisen.effect.JJKEffects;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
@@ -70,7 +72,6 @@ public class EelGrappleProjectile extends JujutsuProjectile implements GeoEntity
         }
 
         this.pulled = target;
-
         this.setDeltaMovement(Vec3.ZERO);
 
         if (this.getTime() >= DURATION) {
@@ -81,6 +82,7 @@ public class EelGrappleProjectile extends JujutsuProjectile implements GeoEntity
         if (owner == null) return;
 
         if (this.pulled != null) {
+            this.pulled.addEffect(new MobEffectInstance(JJKEffects.STAGGER.get(), 2, 0, false, false, false));
             if (this.pulled.isRemoved() || this.pulled.isDeadOrDying()) {
                 this.discard();
                 return;
