e7ceabc4c354289c3569ad17c11d028ee1e00786
wood-m-corp <148374240+wood-m-corp@users.noreply.github.com>
2025-11-08 18:26:22 -0500

    inv curse double entry fixed
    as stated

--- src/main/java/radon/jujutsu_kaisen/JujutsuKaisen.java
+++ src/main/java/radon/jujutsu_kaisen/JujutsuKaisen.java
@@ -103,7 +103,6 @@ public class JujutsuKaisen {
         ItemBlockRenderTypes.setRenderLayer(JJKBlocks.SHINING_SEA_OF_FLOWERS_DECORATION_THREE.get(), RenderType.cutout());
         ItemBlockRenderTypes.setRenderLayer(JJKBlocks.SHINING_SEA_OF_FLOWERS_DECORATION_FOUR.get(), RenderType.cutout());
 
-         CuriosRendererRegistry.register(JJKItems.INVENTORY_CURSE.get(), InventoryCurseRenderer::new);
         CuriosRendererRegistry.register(JJKItems.INVENTORY_CURSE.get(), InventoryCurseRenderer::new);
         CuriosRendererRegistry.register(JJKItems.BLINDFOLD.get(), BlindfoldRenderer::new);
     }
