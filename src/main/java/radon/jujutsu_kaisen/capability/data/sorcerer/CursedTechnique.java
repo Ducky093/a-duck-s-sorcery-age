e8b51f6e2ba44809c635a7c176d0cb21e581fdb7
Ducky093 <declynwayne.edwards@gmail.com>
2026-08-11 09:56:51 -0700

    Update CursedTechnique.java

--- src/main/java/radon/jujutsu_kaisen/capability/data/sorcerer/CursedTechnique.java
+++ src/main/java/radon/jujutsu_kaisen/capability/data/sorcerer/CursedTechnique.java
@@ -51,7 +51,7 @@ public enum CursedTechnique {
     MYTHICAL_BEAST_AMBER(null,JJKAbilities.MYTHICAL_BEAST_AMBER.get(), JJKAbilities.EMF_BLAST.get(), JJKAbilities.ELECTRIC_BEAM.get() ),
     TECHNIQUELESS(null),
     miracles(JJKAbilities.miracle.get()),
-    STAR_RAGE(JJKAbilities.overdrive.get()),
+    STAR_RAGE(JJKAbilities.OVERDRIVE.get()),
     PROJECTION_SORCERY(JJKAbilities.TIME_CELL_MOON_PALACE.get(), JJKAbilities.PROJECTION_SORCERY.get(), JJKAbilities.TWENTY_FOUR_FRAME_RULE.get(), JJKAbilities.AIR_FRAME.get());
     
     private final @Nullable Ability domain;
