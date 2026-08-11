d24dfb280e24b5c5740cbfc8cbbbff5b0e509c45
Ducky093 <declynwayne.edwards@gmail.com>
2026-08-10 20:42:59 -0700

    Update SorcererData.java

--- src/main/java/radon/jujutsu_kaisen/capability/data/sorcerer/SorcererData.java
+++ src/main/java/radon/jujutsu_kaisen/capability/data/sorcerer/SorcererData.java
@@ -742,11 +742,11 @@ public class SorcererData implements ISorcererData {
     }
     @Override
     public float getmaximumstar_rage_output() {
-        float star_rage_output 1.0F
+        float star_rage_output 1.0F;
         if (this.toggled.contains(JJKAbilities.Overdrive.get() )) { 
-            star_rage_output = 5.0F
+            star_rage_output = 5.0F;
                 }
-        )
+    }
     @Override
     public float getMaximumOutput() {
         float output = 1.0F;
