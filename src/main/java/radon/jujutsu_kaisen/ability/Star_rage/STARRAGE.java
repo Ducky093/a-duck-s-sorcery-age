28dcde188d7e7c8bb4efab41bdd570c49b79129b
Ducky093 <declynwayne.edwards@gmail.com>
2026-08-16 11:38:39 -0700

    Update STARRAGE.java

--- src/main/java/radon/jujutsu_kaisen/ability/Star_rage/STARRAGE.java
+++ src/main/java/radon/jujutsu_kaisen/ability/Star_rage/STARRAGE.java
@@ -52,8 +52,8 @@ public class STARRAGE extends Ability implements Ability.IToggled {
     }
 
     @Override
-   
     public float getCost(LivingEntity owner) {
+        float star_rage_output = 0.0F;
         return Math(this.getstar_rage_output + 0.0F);
     }
 
