5a7249cc1d364e867727a3d8a524acc34b4b208d
Ducky093 <declynwayne.edwards@gmail.com>
2026-08-26 18:05:39 -0700

    Update GodsWill.java

--- src/main/java/radon/jujutsu_kaisen/ability/angel/GodsWill.java
+++ src/main/java/radon/jujutsu_kaisen/ability/angel/GodsWill.java
@@ -41,6 +41,7 @@ public class GodsWill extends DomainExpansion implements DomainExpansion.IClosed
     @Override
     public void onHitEntity(DomainExpansionEntity domain, LivingEntity owner, LivingEntity entity, boolean instant) {
         super.onHitEntity(domain, owner, entity, instant);
+        int burnout = cap.getBurnout();
          cap.setburnout(1);
         }
 
