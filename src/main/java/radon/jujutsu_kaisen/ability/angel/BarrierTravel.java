d40fa2ea32a7fc6218a9a0714af9056c186cd353
Ducky093 <declynwayne.edwards@gmail.com>
2026-08-31 13:11:19 -0700

    Update BarrierTravel.java

--- src/main/java/radon/jujutsu_kaisen/ability/angel/BarrierTravel.java
+++ src/main/java/radon/jujutsu_kaisen/ability/angel/BarrierTravel.java
@@ -69,11 +69,6 @@ public class BarrierTravel extends Ability implements Ability.IToggled {
     }
         @Override
     public boolean isTechnique(LivingEntity owner) {
-        ISorcererData cap = owner.getCapability(SorcererDataHandler.INSTANCE).resolve().orElseThrow();
-        if (cap.hasTrait(Trait.HEAVENLY_RESTRICTION)) {
-            return false;
-            
-        }
         return true;
     }
 
